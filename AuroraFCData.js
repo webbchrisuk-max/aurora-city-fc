17d0c1c763aaae8aa95b76deb4168e07fc0d43c8
webbchrisuk-max <webbchris.uk@icloud.com>
2026-06-15 14:29:30 +0100

    Add AuroraFCData.js with club information and budget

---
+++ AuroraFCData.js
@@ -0,0 +1,18 @@
+const AURORA_FC = {
+  clubName: "Aurora City FC",
+
+  pages: {
+    manager: "AuroraCityFC_ManagerDashboard.html",
+    transfer: "AuroraCityFC_TransferCentre.html",
+    boardroom: "AuroraCityFC_Boardroom.html",
+    squad: "AuroraCityFC_SquadHub.html"
+  },
+
+  topTargets: ["RGL", "RKT", "ULVR", "TRIG"],
+
+  budget: {
+    nextPayday: 500,
+    reserve: 50,
+    available: 450
+  }
+};
