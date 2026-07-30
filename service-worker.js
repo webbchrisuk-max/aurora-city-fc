8ca8737d387cd78298196cebe8e56ef82d89eef9
webbchrisuk-max <webbchris.uk@icloud.com>
2026-07-30 22:36:36 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,74 @@
-const CACHE_NAME = 'aurora-city-fc-v2026-07-25-safe-3';
+const CACHE_NAME = "aurora-city-fc-v2026-07-30-app-1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./AuroraCityFC_GameShell.html",
+  "./assets/aurora-city-fc/icons/icon-192.png",
+  "./assets/aurora-city-fc/icons/icon-512.png"
+];
 
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_FILES);
+    })
+  );
+
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => {
+        return Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        );
+      })
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  if (new URL(request.url).origin !== self.location.origin) {
+    return;
+  }
 
   event.respondWith(
-    fetch(event.request)
+    fetch(request)
       .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy).catch(() => {});
-        });
+        if (response && response.ok) {
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(request, copy).catch(() => {});
+          });
+        }
+
         return response;
       })
-      .catch(() => caches.match(event.request))
+      .catch(async () => {
+        const cachedResponse = await caches.match(request);
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        if (request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+
+        return Response.error();
+      })
   );
 });
